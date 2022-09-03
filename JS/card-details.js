5f93b45a2cd8c55b8875a7a77dcffdd83cc5f05c
saifsammy <saifsammy653@gmail.com>
2022-09-04 03:29:32 +0600

    code clean

--- JS/card-details.js
+++ JS/card-details.js
@@ -42,24 +42,3 @@ const displayCard = (updateCards) => {
 
 }
 cardDetails();
-
-
-
-// for (const cards in updateCards) {
-    //     const cardDetail = document.createElement('div');
-    //     cardDetail.classList.add('col');
-    //     cardDetail.innerHTML = `
-    //     <div class="card h-100">
-    //         <img src="" class="card-img-top" alt="...">
-    //         <div class="card-body">
-    //             <h5 class="card-title">${cards._id}</h5>
-    //             <p class="card-text"></p>
-    //         </div>
-    //         <div class="card-footer">
-    //             <small class="text-muted">Last updated 3 mins ago</small>
-    //         </div>
-    //     </div>
-    //     `;
-    //     cardContainer.appendChild(cardDetail);
-    //     console.log(cards);
-    // }
