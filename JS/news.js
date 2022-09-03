47541af7c1bd99c27ade9d5918c628d41e180808
saifsammy <saifsammy653@gmail.com>
2022-09-03 15:17:21 +0600

    first commit

---
+++ JS/news.js
@@ -0,0 +1,27 @@
+const loadNews = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        displayNews(data.data.news_category);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+loadNews();
+const displayNews = (updateNews) => {
+    const navContainer = document.getElementById('nav-container');
+
+    updateNews.forEach(news => {
+        const navBar = document.createElement('a');
+        navBar.classList.add('col');
+        navBar.innerHTML = `
+        
+        <a href="">${news.category_name}</a>
+        `;
+        navContainer.appendChild(navBar);
+    });
+
+    console.log(updateNews);
+}
